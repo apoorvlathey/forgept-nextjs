ec0b17a15f4c734471fc96c85765b7c191bea351
apoorvlathey <latheyapoorv@gmail.com>
2023-06-24 04:14:05 +0530

    chakra init

---
+++ style/theme.ts
@@ -0,0 +1,39 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    greenLight: "#19cb01",
+    greenDark: "#16b201",
+    greenDarker: "#129801",
+    yellow: "#EDF676",
+  },
+};
+
+const fonts = {
+  brand: "Poppins",
+};
+
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      html: {
+        scrollBehavior: "smooth",
+      },
+      body: {
+        bg: "black",
+        color: "white",
+        fontFamily: "Poppins",
+      },
+    },
+  },
+  config,
+  colors,
+  fonts,
+});
+
+export default theme;
