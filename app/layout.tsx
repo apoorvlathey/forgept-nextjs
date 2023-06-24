95a1813dc5bedebca2a5d206b242e45bf658de77
apoorvlathey <latheyapoorv@gmail.com>
2023-06-24 07:23:15 +0530

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,29 @@
+import { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { Providers } from "./providers";
 import Analytics from "@/components/Analytics";
 
 const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
-export const metadata = {
+const _metadata = {
   title: "ForGePT",
-  description: "Solve your Foundry related queries with this AI bot!",
+  description: "Solve your Foundry related queries with this ChatGPT bot!",
+};
+
+export const metadata: Metadata = {
+  title: _metadata.title,
+  description: _metadata.description,
+  twitter: {
+    card: "summary_large_image",
+    creator: "@apoorvlathey",
+    title: _metadata.title,
+    description: _metadata.description,
+  },
+  openGraph: {
+    type: "website",
+    title: _metadata.title,
+    description: _metadata.description,
+  },
 };
 
 export default function RootLayout({
